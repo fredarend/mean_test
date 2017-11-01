275b51b1031f9b3cd7de23e5000bb8d044f5f560
fredarend <fredericobinsfeld@gmail.com>
2017-11-01 17:46:02 -0200

    Back

--- backend/api/acoes/acoesService.js
+++ backend/api/acoes/acoesService.js
@@ -1,5 +1,16 @@
 const acoes = require('./acoes')
 
 acoes.methods(['get', 'post', 'put', 'delete'])
+acoes.updateOptions({new: true, runValidators: true})
+
+acoes.route('count', function(req, res, next) {
+  acoes.count(function(error, value) {
+    if(error) {
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json({value})
+    }
+  })
+})
 
 module.exports = acoes
